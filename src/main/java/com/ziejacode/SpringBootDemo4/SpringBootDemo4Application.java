903a987551e8c0edf48d4d6b91cafdbc2e046c63
Marty <ziejacode@gmail.com>
2022-02-11 06:38:22 +0000

    Initial commit

---
+++ src/main/java/com/ziejacode/SpringBootDemo4/SpringBootDemo4Application.java
@@ -0,0 +1,13 @@
+package com.ziejacode.SpringBootDemo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ziejacode/SpringBootDemo4/SpringBootDemo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziejacode.SpringBootDemo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
