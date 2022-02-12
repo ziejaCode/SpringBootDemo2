2c324564546d70de9a6385dd4ab02ec3ae690909
Marty <ziejacode@gmail.com>
2022-02-12 10:30:25 +0000

    Model update

---
+++ src/main/java/com/ziejacode/SpringBootDemo4/models/Author.java
@@ -0,0 +1,4 @@
+package com.ziejacode.SpringBootDemo4.models;
+
+public class Author {
+}
---
+++ src/main/java/com/ziejacode/SpringBootDemo4/models/Book.java
@@ -0,0 +1,45 @@
+package com.ziejacode.SpringBootDemo4.models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Book {
+
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    private Set<Author>authors = new HashSet<Author>();
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
